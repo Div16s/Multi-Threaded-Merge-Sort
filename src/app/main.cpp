be5e59b22b57715228b164610b7fa6f46e28bf20
Div16s <divyankarshah1602@gmail.com>
2025-01-09 17:05:02 +0530

    Completed MultiThreadedMergeSort Algorithm

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,27 +1,38 @@
 # include <iostream>
 # include <memory> // for smart pointers
-#include <chrono> // for high_resolution_clock
-#include "MergeSort/mergeSort.hpp"
+# include <chrono> // for high_resolution_clock
+# include "MergeSort/mergeSort.hpp"
+# include "MultiThreadedMergeSort/multiThreadedMergeSort.hpp"
 using namespace std;
 
-const int SIZE = 1000;
+const int SIZE = 10000000;
 
 int main(int argc, char* argv[]){
-    vector<int> arr(SIZE);
+    vector<int> arr1(SIZE), arr2(SIZE);
     for(int i = 0; i < SIZE; i++) {
-        arr[i] = rand()%SIZE;
+        arr1[i] = rand()%SIZE;
+        arr2[i] = rand()%SIZE;
     }
 
     // cout << "Unsorted array: ";
     // for(int i = 0; i < SIZE; i++) cout << arr[i] << " ";
     // cout << endl;
 
-    unique_ptr<MergeSort> mergeSort = make_unique<MergeSort>(arr);
+    unique_ptr<MergeSort> mergeSort = make_unique<MergeSort>(arr1);
     auto start = chrono::high_resolution_clock::now();
     mergeSort->sort();
     auto end = chrono::high_resolution_clock::now();
-    double duration = chrono::duration<double>(end-start).count();
+    auto duration = chrono::duration<double>(end-start).count();
     cout << "Time taken to sort the array using normal merge sort algorithm: " << duration << " seconds" << endl;
 
+    unique_ptr<MultiThreadedMergeSort> multiThreadedMergeSort = make_unique<MultiThreadedMergeSort>(arr2);
+    start = chrono::high_resolution_clock::now();
+    multiThreadedMergeSort->merge_sort();
+    end = chrono::high_resolution_clock::now();
+    duration = chrono::duration<double>(end-start).count();
+    cout << "Time taken to sort the array using multi-threaded merge sort algorithm: " << duration << " seconds" << endl;
+    // cout << "Sorted array: ";
+    // for(int i = 0; i < SIZE; i++) cout << arr2[i] << " ";
+    // cout << endl;
     return 0;
 }
